97cf48b4f6d885644284129ba6b501089b5d8b7b
Mohamed Chiguer <chiguermohamed41@gmail.com>
2026-03-01 03:14:05 +0100

    Adaptation API pour Vercel avec memoryStorag32e

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); 
-
+app.use(express.static(path.join(__dirname, "public"))); 
 app.use(session({
   secret: 'secret123',
   resave: false,
